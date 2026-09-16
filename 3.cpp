d62f95832eefc901b92c63c3a1464e577707e2b6
jorgezm238 <8autor@gmail.com>
2026-09-16 10:57:56 +0200

    Create 3.cpp

---
+++ 3.cpp
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int x=3;
+    int *p = &x;
+    
+    x = 7;
+    *p = *p +2;
+    
+    printf("%d\n",x);
+    printf("%d\n",*p); 
+    
+    
+    
+}
